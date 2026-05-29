7d45dc394da4fb2216d71e19fa4636afedf4ae94
Hector Perez <narsteven23@gmail.com>
2026-05-29 11:02:24 -0400

    Create 2.1.C

---
+++ Tarea 2 de PM/2.1/2.1.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Promedio curso.
+El programa, al recibir como dato al promedio de un alumno en un curso universitario, escribe aprobado si su promedio es mayor o igual a 6.
+
+Pro: Variable de tipo real. */
+
+void main(void)
+{
+    float PRO;
+    printf("ingrese el promedio del alumno: ");
+    scanf("%f", &PRO);
+    if (PRO >= 6)
+       printf("\nAprobado\n");
+}
