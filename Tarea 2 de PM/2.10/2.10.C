0177dad1787017da0abd79ee4a066478da31428f
Hector Perez <narsteven23@gmail.com>
2026-05-30 00:14:26 -0400

    Mas

---
+++ Tarea 2 de PM/2.10/2.10.C
@@ -0,0 +1,20 @@
+#include <stdio.h?
+#include <math.h>
+
+/* Par, impar o nulo.
+NUM: Variable de tipo entero. */
+
+int Principal(Vacio)
+{
+    int NUM;
+    Printf("Ingrese el numero:");
+    Scanf("%d", &NUM);
+    si (NUM == 0)
+         Printf("n\nulo");
+    si no,
+          si (¡pum! (-1, NUM) > 0)
+             Printf("\nPar");
+          si no,
+                Printf("\nImpar");
+    Regreso 0;
+}
---
+++ Tarea 2 de PM/2.15/2.15.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/* Spa.
+El programa, al recibir como datos el tipo de tratamiento, la edad y el numero de dias de internacion de un cliente en un spa, calcula el costo total del tratamiento.
+
+TRA, EDA, DIA: variables de tipo entero.
+COS: variable de tipo real. */
+
+int main (void)
+{
+    int TRA, EDA, DIA;
+    float COS;
+    printf("Ingrese el tipo de tratamiento, edad y dias:");
+    scanf("%d %d %d", &TRA, &EDA, &DIA);
+    switch (TRA)
+    {
+        case 1: COS = DIA * 2800; break;
+        case 2: COS = DIA * 1950; break;
+        case 3: COS = DIA * 2500; break;
+        case 4: COS = -1; break;
+    }
+    if (COS != -1)
+    {
+        if (EDA >=60)
+            COS = COS * 0.75;
+        else 
+            if (EDA <= 25)
+                COS = COS * 0.8;
+        printf("\nClave Tratamiento: %d\t Dias: %d\t costo total: $8.2f",
+        TRA, DIA, COS);
+        }
+        else
+            printf("\nLa clave del tratamiento es incorrecta");
+    return 0;
+}
