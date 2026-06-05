9c6f37b0e3cfae8f92fdba6cb087a9e37f4d43f5
Hector Perez <narsteven23@gmail.com>
2026-06-05 10:56:09 -0400

    Tarea 3

---
+++ Tarea 3/Capitulo 4/4.4/4.4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/* Prueba de variables globales, locales y estaticas.
+El programa utiliza funciones en las que se usan diferentes tipos de variables. */
+
+int f1(void);
+int f2(void);
+int f3(void);                    /* Prototipos de funciones. */
+int f4(void);
+
+int K = 3;                      /* Variable global. */
+
+void main(void)
+{
+    int I;
+    for (I = 1; I <= 3; I++)
+    {
+        printf("\nEL resultado de la funcion f1 es: %d", f1());
+        printf("\nEl resultado de la funcion f2 es: %d", f2());
+        printf("\nEl resultado de la funcion f3 es: %d", f3());
+        printf("\nEl resultado de la funcion f4 es: %d", f4());
+        printf("\nEl resultado de la funcion f5 es: %d", f5());
+    }
+}
+int f1(void)
+/* La funcion f2 utiliza la variable local. */
+    {
+        int K = 1;
+        K++;
+        return (K);
+    }
+
+int f2(void)
+/* La funcion f2 utiliza la variable local. */
+    {
+        int K = 1;
+        K++;
+        return(K);
+    }
+
+int f3(void)
+/* La funcion f3 utiliza la variable estatica. */
+    {
+        static int K = 8;
+        K += 2;
+        return (K);
+    }
+
+int f4(void)
+/* La funcion f4 utiliza dos variables con el mismo nombre: local y global. */
+    {
+        int K = 5;
+        K = K + ::K;         /* Uso de la variable local (K) y global (::K) */
+        return(K);
+    }
